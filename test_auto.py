694f4dc94562e3df8ac2d81783ed143911491c91
张渝 <zhangyu@sensecho.com>
2020-04-09 18:24:06 +0800

    输入身高

--- test_auto.py
+++ test_auto.py
@@ -11,20 +11,24 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestAuto():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome("F:\webdriver\chromedriver.exe")
+    # self.driver = webdriver.Chrome("F:\webdriver\chromedriver.exe")
+    self.driver = webdriver.Chrome("D:\chrome\chromedriver.exe")
     self.vars = {}
 
   def test_auto(self):
-      self.driver.get("http://39.98.251.12/www/login")
+      # self.driver.get("http://39.98.251.12/www/login")
+      self.driver.get("http://192.168.1.197/www/login")
       self.driver.maximize_window()
       self.driver.find_element(By.CSS_SELECTOR,".ivu-input-type-text > .ivu-input").click()
-      self.driver.find_element(By.CSS_SELECTOR,".ivu-input-type-text > .ivu-input").send_keys("test")
+      # self.driver.find_element(By.CSS_SELECTOR,".ivu-input-type-text > .ivu-input").send_keys("test")
+      self.driver.find_element(By.CSS_SELECTOR,".ivu-input-type-text > .ivu-input").send_keys("zd")
       self.driver.find_element(By.CSS_SELECTOR,".ivu-input-type-password > .ivu-input").click()
       self.driver.find_element(By.CSS_SELECTOR,".ivu-input-type-password > .ivu-input").send_keys("123456")
       self.driver.find_element(By.CSS_SELECTOR, ".ivu-checkbox-input").click()
       self.driver.find_element(By.CSS_SELECTOR, ".loginBtn > span").click()
-      time.sleep(1)
+      time.sleep(3)
       self.driver.find_element(By.CSS_SELECTOR,".ivu-menu-submenu:nth-child(7) > .ivu-menu-submenu-title").click()
+      time.sleep(1)
       self.driver.find_element(By.CSS_SELECTOR,".ivu-menu-opened .ivu-menu-item:nth-child(1)").click()
       self.driver.find_element(By.CSS_SELECTOR,".f-floatRight > span").click()  # 点击添加患者
       self.driver.find_element(By.CSS_SELECTOR,".ivu-form-item-required .ivu-input:nth-child(2)").click()
@@ -33,7 +37,7 @@ class TestAuto():
       self.driver.find_element(By.CSS_SELECTOR,".ivu-form-item-content > .ivu-input-type-text > .ivu-input:nth-child(3)").send_keys("自动6号")
       self.driver.find_element(By.CSS_SELECTOR,".ivu-radio-group-item:nth-child(2)").click()
 
-      # 选择日期
+      # 选择出生日期
       self.driver.find_element(By.CSS_SELECTOR,".ivu-form-item-content > .ivu-date-picker").click()
       self.driver.find_element(By.CSS_SELECTOR,".ivu-picker-panel-body > .ivu-date-picker-header > span:nth-child(3) > span:nth-child(1)").click()
       self.driver.find_element(By.CSS_SELECTOR,".ivu-picker-panel-body > .ivu-date-picker-header > span:nth-child(3) > .ivu-icon-ios-arrow-forward").click()
@@ -41,8 +45,12 @@ class TestAuto():
       self.driver.find_element(By.CSS_SELECTOR,".ivu-picker-panel-body > .ivu-date-picker-header > span:nth-child(3) > .ivu-icon-ios-arrow-forward").click()
       self.driver.find_element(By.CSS_SELECTOR,".ivu-date-picker-cells-year > span:nth-child(6)").click()
       self.driver.find_element(By.CSS_SELECTOR,".ivu-date-picker-cells-month > span:nth-child(4)").click()
-
       self.driver.find_element(By.CSS_SELECTOR,".ivu-date-picker-cells:nth-child(1) >.ivu-date-picker-cells-cell:nth-child(37) > em").click()
+
+      # self.driver.find_element(By.CSS_SELECTOR,".ivu-form-label-right > .ivu-row:nth-child(3) > .ivu-col:nth-child(1)  .ivu-input-number-input").click() # 身高
+      self.driver.find_element(By.CSS_SELECTOR,".ivu-form-label-right > .ivu-row:nth-child(3) > .ivu-col:nth-child(1)  .ivu-input-number-input").send_keys("120") # 身高
+      time.sleep(100)
+
 
 
 
